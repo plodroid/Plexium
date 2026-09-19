f594f59951bb456cbb2e62f37744c96a45ca428e
plodroid <138373351+plodroid@users.noreply.github.com>
2026-09-19 12:25:18 +0100

    Add ImgShock ad configuration

---
+++ imgshock/config.js
@@ -0,0 +1,12 @@
+window.IMGSHOCK_CONFIG = {
+  adsenseClient: "",
+  autoAds: true,
+  adSlots: {
+    top: "",
+    left: "",
+    right: "",
+    inline1: "",
+    inline2: "",
+    footer: ""
+  }
+};
